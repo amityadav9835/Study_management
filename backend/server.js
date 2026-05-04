2bc869f17e6da1ad9b9a216b582c750ba8200d4d
Amit Kumar <amitkumar9973253016@gmail.com>
2026-05-04 11:24:43 +0530

    change cors ploicy

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json()); // to parse json body
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || "*",
+        origin: "*",
         credentials: true
     })
 );
